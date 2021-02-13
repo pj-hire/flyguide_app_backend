d65def67939b6cd3e8913636d1ef9423cfd00d84
PJ Hire <phire1224@gmail.com>
2021-02-13 12:46:17 -0600

    gitignore

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const mysql = require('mysql');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const dbCred = require('./private/db_credentials')
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'Mycell=816',
-  database : 'flyguide_app'
-});
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'Mycell=816',
+//   database : 'flyguide_app'
+// });
+
+var connection = mysql.createConnection(dbCred);
 
 connection.connect();
 
